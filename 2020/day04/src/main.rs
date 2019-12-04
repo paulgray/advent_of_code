38bc8aab23996eade84496211bea8798a11c4baa
Michal Ptaszek <michal@ptaszek.net>
2019-12-03 23:42:05 -0800

    Day 4.2 done

--- 2020/day04/src/main.rs
+++ 2020/day04/src/main.rs
@@ -32,13 +32,32 @@ fn is_valid_password(n : i32) -> bool {
         return false;
     }
 
+    // the two adjacent matching digits are not part of a larger group of matching digits
+    let mut exact_doubles = false;
+    let a = n % 10;
+    let b = (n / 10) % 10;
+    let c = (n / 100) % 10;
+    let d = (n / 1000) % 10;
+    let e = (n / 10000) % 10;
+    let f = (n / 100000) % 10;
+    if (a == b && a != c) ||
+       (a != b && b == c && b != d) ||
+       (b != c && c == d && d != e) || 
+       (c != d && d == e && e != f) ||
+       (d != e && e == f) {
+           exact_doubles = true;
+    }
+    if !exact_doubles {
+        return false;
+    }
+
     println!("{}", n);
     return true;
 }
 
 fn main() {
     let mut count = 0;
-    for i in 178416..676462 {
+    for i in 178416..=676461 {
         if is_valid_password(i) {
             count += 1;
         }
@@ -49,7 +68,7 @@ fn main() {
 
 #[test]
 fn test() {
-    assert!(is_valid_password(111111));
-    assert!(!is_valid_password(223450));
-    assert!(!is_valid_password(123789));
+    assert!(is_valid_password(112233));
+    assert!(!is_valid_password(123444));
+    assert!(is_valid_password(111122));
 }
