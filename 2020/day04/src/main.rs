c6d4eca98577b0b23b7fd0a1fa0a93a6e66aa36a
Michal Ptaszek <michal@ptaszek.net>
2019-12-04 10:35:31 -0800

    Improved day 4.2

--- 2020/day04/src/main.rs
+++ 2020/day04/src/main.rs
@@ -1,45 +1,22 @@
 fn is_valid_password(n : i32) -> bool {
-    // at least 2 adjacent same digits
-    let mut same = false;
-    let mut temp : i32 = n;
-    while temp > 0 {
-        if (temp % 10) == ((temp / 10) % 10) {
-            same = true;
-            break;
-        }
-        temp = temp / 10;
-    }
-    if !same {
-        return false;
-    }
-
-    // digits never decrease
-    let mut increase = false;
-    temp = n / 10;
-    let mut last = n % 10;
-    let mut current : i32;
-    while temp > 0 {
-        current = temp % 10;
-        if current > last {
-            increase = true;
-            break;
-        }
-
-        last = current;
-        temp = temp / 10;
-    }
-    if increase {
-        return false;
-    }
-
-    // the two adjacent matching digits are not part of a larger group of matching digits
-    let mut exact_doubles = false;
     let a = n % 10;
     let b = (n / 10) % 10;
     let c = (n / 100) % 10;
     let d = (n / 1000) % 10;
     let e = (n / 10000) % 10;
     let f = (n / 100000) % 10;
+
+    // digits never decrease
+    if f > e ||
+       e > d ||
+       d > c || 
+       c > b || 
+       b > a {
+           return false;
+    }
+
+    // the two adjacent matching digits are not part of a larger group of matching digits
+    let mut exact_doubles = false;
     if (a == b && a != c) ||
        (a != b && b == c && b != d) ||
        (b != c && c == d && d != e) || 
