11d7c09e761d769584809686ed52d04f1429076b
Michal Ptaszek <michal@ptaszek.net>
2019-12-05 23:44:00 -0800

    Day 6.2 done

--- 2020/day06/src/main.rs
+++ 2020/day06/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashMap;
 
-fn count_orbits(lines : Vec<&str>) -> i32 {
+fn count_orbits(lines : Vec<&str>) -> (i32, i32) {
     let mut planets : HashMap<&str, &str> = HashMap::new();
 
     lines.iter().for_each(|s| { 
@@ -27,7 +27,36 @@ fn count_orbits(lines : Vec<&str>) -> i32 {
         }
     }
 
-    return count;
+    let mut my_chain = Vec::new();
+    let mut santa_chain = Vec::new();
+    let mut orbit = planets.get(&"YOU").unwrap();
+    loop {
+        my_chain.push(orbit);
+        if !planets.contains_key(orbit) {
+            break;
+        }
+        orbit = planets.get(orbit).unwrap();
+    }
+    
+    orbit = planets.get(&"SAN").unwrap();
+    loop {
+        santa_chain.push(orbit);
+        if !planets.contains_key(orbit) {
+            break;
+        }
+        orbit = planets.get(orbit).unwrap();
+    }
+
+    let mut xfers : i32 = 0;
+    for (idx, planet) in my_chain.iter().enumerate() {
+        let common = santa_chain.iter().position(|x| x == planet);
+        if common.is_some() {
+            xfers = idx as i32 + common.unwrap() as i32;
+            break;
+        }
+    }
+
+    return (count, xfers);
 }
 
 fn main() {
@@ -37,12 +66,12 @@ fn main() {
     file.read_to_string(&mut input).unwrap();
     let planets = input.split("\n").collect::<Vec<&str>>();
 
-    let count = count_orbits(planets);
-    println!("Orbits: {}", count);
+    let (count, xfers) = count_orbits(planets);
+    println!("Orbits: {}, {}", count, xfers);
 }
 
 #[test]
 fn test() {
-    let input = vec!["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"];
-    assert_eq!(42, count_orbits(input));
+    let input = vec!["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"];
+    assert_eq!((54, 4), count_orbits(input));
 }
