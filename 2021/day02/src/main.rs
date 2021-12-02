37d313c47bf3d2ee5443dd2277987f369dc9052b
Michal Ptaszek <michal@ptaszek.net>
2021-12-02 11:08:27 -0600

    Day02.2 done

--- 2021/day02/src/main.rs
+++ 2021/day02/src/main.rs
@@ -14,9 +14,29 @@ fn star1(input: &str) {
     println!("{}", coords.0 * coords.1);
 }
 
+fn star2(input: &str) {
+    let coords = input
+        .trim()
+        .split("\n")
+        .map(|line| line.split(" ").collect::<Vec<&str>>())
+        .map(|i| (i[0], i[1].parse::<u32>().unwrap_or_default()))
+        .fold(
+            (0, 0, 0),
+            |(horizontal, depth, aim), (dir, modifier)| match dir {
+                "forward" => (horizontal + modifier, depth + aim * modifier, aim),
+                "down" => return (horizontal, depth, aim + modifier),
+                "up" => return (horizontal, depth, aim - modifier),
+                &_ => return (horizontal, depth, aim),
+            },
+        );
+
+    println!("{}", coords.0 * coords.1);
+}
+
 fn main() {
     let contents =
         std::fs::read_to_string("test1").expect("Something went wrong when reading the input file");
 
     star1(&contents);
+    star2(&contents);
 }
