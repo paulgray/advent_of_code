a22387500d6e24c29baac589878d58f39c182be9
Michal Ptaszek <michal@ptaszek.net>
2021-12-07 13:31:34 -0600

    Day07.2 done

--- 2021/day07/src/main.rs
+++ 2021/day07/src/main.rs
@@ -1,8 +1,14 @@
-fn cost(position: i32, positions: &Vec<i32>) -> i32 {
-    let mut acc: i32 = 0;
+fn cost(position: i32, positions: &Vec<i32>, costs: &Vec<u128>) -> u128 {
+    let mut acc: u128 = 0;
 
     for p in positions {
-        acc += (position - p).abs();
+        let distance = (position - p).abs();
+
+        if distance > 10000 {
+            // cost is way too high
+            return u128::MAX;
+        }
+        acc += costs[distance as usize];
     }
 
     return acc;
@@ -19,10 +25,17 @@ fn star1(input: &str) {
     // let's find the best position
     let mut proposed = *positions.first().unwrap();
     let last = *positions.last().unwrap();
-    let mut min_fuel = i32::MAX;
+    let mut min_fuel = u128::MAX;
+
+    // compute costs for the first 10000 positions or so
+    let mut costs: Vec<u128> = Vec::new();
+    costs.push(0);
+    for n in 1..=10000 {
+        costs.push(n + costs.last().unwrap());
+    }
 
     while proposed < last {
-        let fuel = cost(proposed, &positions);
+        let fuel = cost(proposed, &positions, &costs);
         if fuel < min_fuel {
             println!("Found new min fuel! Position {}, cost {}", proposed, fuel);
             min_fuel = fuel;
