9c795aa12facebe7b0ae03bb21e246ca5de44288
Michal Ptaszek <michal@ptaszek.net>
2021-12-09 14:13:33 -0600

    Day 09.2 done

--- 2021/day09/src/main.rs
+++ 2021/day09/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn print_map(map: &Vec<Vec<u32>>) {
     for row in map {
         for cell in row {
@@ -86,6 +88,7 @@ fn star1(input: &str) {
 
     // find local minima
     let mut risks = 0;
+    let mut basins: HashMap<(usize, usize), u32> = HashMap::new();
     for y in 1..max_y - 1 {
         let prev_row = map.get(y - 1).unwrap();
         let curr_row = map.get(y).unwrap();
@@ -99,17 +102,91 @@ fn star1(input: &str) {
                 && val < curr_row.get(x - 1).unwrap()
                 && val < curr_row.get(x + 1).unwrap()
             {
+                let coords = (x, y);
+                basins.insert(coords, 0);
                 risks += 1 + val;
             }
         }
     }
 
     println!("Risk levels = {}", risks);
+
+    // also, each point will belong to a basin
+    //    point coords -> basin coords
+    let mut points: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+    let mut walls: Vec<(usize, usize)> = Vec::new();
+    for m in basins.keys() {
+        points.insert(*m, *m);
+    }
+
+    // iterate until we have some unassigned points left
+    let counter_desired = (max_x - 2) * (max_y - 2);
+    while points.len() < counter_desired {
+        for y in 1..max_y - 1 {
+            let prev_row = map.get(y - 1).unwrap();
+            let curr_row = map.get(y).unwrap();
+            let next_row = map.get(y + 1).unwrap();
+            for x in 1..max_x - 1 {
+                let coords = (x, y);
+                if points.contains_key(&coords) {
+                    // we already tracked this point
+                    continue;
+                }
+
+                let val = curr_row.get(x).unwrap();
+                if *val == 9 {
+                    // this does not belong to any basin
+                    points.insert(coords, coords);
+                    walls.push(coords);
+                }
+
+                // the point is not assigned to anything
+                // if any of the neighboring points is in
+                // a basin and is not a 9 -> let's move it
+                // to that basin
+                let top = prev_row.get(x).unwrap();
+                if val > top && points.contains_key(&(x, y - 1)) && *top < 9 {
+                    points.insert(coords, *points.get(&(x, y - 1)).unwrap());
+                }
+
+                let bottom = next_row.get(x).unwrap();
+                if val > bottom && points.contains_key(&(x, y + 1)) && *bottom < 9 {
+                    points.insert(coords, *points.get(&(x, y + 1)).unwrap());
+                }
+
+                let left = curr_row.get(x - 1).unwrap();
+                if val > left && points.contains_key(&(x - 1, y)) && *left < 9 {
+                    points.insert(coords, *points.get(&(x - 1, y)).unwrap());
+                }
+
+                let right = curr_row.get(x + 1).unwrap();
+                if val > right && points.contains_key(&(x + 1, y)) && *right < 9 {
+                    points.insert(coords, *points.get(&(x + 1, y)).unwrap());
+                }
+            }
+        }
+    }
+
+    // count basin sizes
+    for (point, basin) in points {
+        if walls.contains(&point) {
+            continue;
+        }
+
+        basins.insert(basin, basins.get(&basin).unwrap() + 1);
+    }
+
+    // get basin sizes
+    let mut sizes: Vec<&u32> = basins.values().collect();
+    sizes.sort();
+    sizes.reverse();
+
+    println!("Sizes: {}", sizes[0] * sizes[1] * sizes[2]);
 }
 
 fn main() {
     let contents =
-        std::fs::read_to_string("test0").expect("Something went wrong when reading the input file");
+        std::fs::read_to_string("test1").expect("Something went wrong when reading the input file");
 
     star1(&contents);
 }
