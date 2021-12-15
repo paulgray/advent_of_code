e149918c0f85e9cd32fbbb1e9d13b184fca94946
Michal Ptaszek <michal@ptaszek.net>
2021-12-15 14:51:51 -0600

    Day 15.1 done

---
+++ 2021/day15/src/main.rs
@@ -0,0 +1,142 @@
+use std::collections::HashMap;
+
+fn print_map(map: &Vec<Vec<u32>>) {
+    for row in map {
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
+    }
+}
+
+fn build_map(max_x: usize, max_y: usize) -> Vec<Vec<u32>> {
+    let mut map: Vec<Vec<u32>> = Vec::new();
+
+    // construct the initial map with empty values
+    let mut row: Vec<u32> = Vec::new();
+    let mut i = 0;
+    while i < max_x {
+        row.push(0);
+        i += 1;
+    }
+
+    let mut j = 0;
+    while j < max_y {
+        map.push(row.clone());
+        j += 1;
+    }
+
+    return map;
+}
+
+fn fill_map(input: &Vec<&str>, map: &mut Vec<Vec<u32>>) {
+    for (y, line) in input.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            map.get_mut(y).unwrap()[x] = c.to_digit(10).unwrap();
+        }
+    }
+}
+
+fn find_path(
+    pos: (usize, usize),
+    max_x: usize,
+    max_y: usize,
+    unvisited: &mut Vec<(usize, usize)>,
+    costs: &mut HashMap<(usize, usize), u32>,
+    map: &Vec<Vec<u32>>,
+) {
+    if unvisited.len() % 10 == 0 {
+        println!("Unvisited len: {}", unvisited.len());
+    }
+
+    // for the current node, consider all unvisited neighbors
+    let mut neighbors: Vec<(usize, usize)> = Vec::new();
+    if pos.0 > 0 && unvisited.contains(&(pos.0 - 1, pos.1)) {
+        neighbors.push((pos.0 - 1, pos.1));
+    }
+    if pos.0 < max_x - 1 && unvisited.contains(&(pos.0 + 1, pos.1)) {
+        neighbors.push((pos.0 + 1, pos.1));
+    }
+    if pos.1 > 0 && unvisited.contains(&(pos.0, pos.1 - 1)) {
+        neighbors.push((pos.0, pos.1 - 1));
+    }
+    if pos.1 < max_y - 1 && unvisited.contains(&(pos.0, pos.1 + 1)) {
+        neighbors.push((pos.0, pos.1 + 1));
+    }
+    // calculate costs
+    let current_cost = *costs.get(&pos).unwrap();
+    for neighbor in neighbors {
+        let cost = current_cost + map[neighbor.1][neighbor.0];
+        let existing_cost = costs.get(&neighbor).unwrap();
+
+        if cost < *existing_cost {
+            costs.insert(neighbor, cost);
+        }
+    }
+
+    let idx = unvisited.iter().position(|x| *x == pos).unwrap();
+    unvisited.remove(idx);
+
+    // check for reaching the end
+    if pos != (max_x - 1, max_y) {
+        // selected an unvisited node that's marked with the
+        // lowest cost and continue
+        let mut next = (0, 0);
+        let mut lowest_cost = u32::MAX;
+
+        for (node, cost) in costs.clone() {
+            if unvisited.contains(&node) && cost < lowest_cost {
+                next = node;
+                lowest_cost = cost;
+            }
+        }
+
+        if next != (0, 0) {
+            find_path(next, max_x, max_y, unvisited, costs, &map);
+        }
+    }
+}
+
+fn star1(input: &str) {
+    let lines = input
+        .trim()
+        .split("\n")
+        .map(|line| line.trim())
+        .collect::<Vec<&str>>();
+
+    let max_x = lines[0].len();
+    let max_y = lines.len();
+
+    let mut map: Vec<Vec<u32>> = build_map(max_x, max_y);
+    fill_map(&lines, &mut map);
+
+    print_map(&map);
+
+    // we're going from (0, 0) to (max_x-1, max_y-1)
+    let mut unvisited: Vec<(usize, usize)> = Vec::new();
+    let mut costs: HashMap<(usize, usize), u32> = HashMap::new();
+
+    // mark all nodes as unvisited and set the initial costs
+    for x in 0..max_x {
+        for y in 0..max_y {
+            unvisited.push((x, y));
+            costs.insert((x, y), u32::MAX);
+        }
+    }
+    costs.insert((0, 0), 0);
+
+    find_path((0, 0), max_x, max_y, &mut unvisited, &mut costs, &map);
+    println!(
+        "Cost to {},{}: {}",
+        max_x - 1,
+        max_y - 1,
+        costs.get(&(max_x - 1, max_y - 1)).unwrap()
+    );
+}
+
+fn main() {
+    let contents =
+        std::fs::read_to_string("test1").expect("Something went wrong when reading the input file");
+
+    star1(&contents);
+}
