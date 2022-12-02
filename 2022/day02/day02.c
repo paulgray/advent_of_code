b3bff4d176cecca6f80663b3b26619c300ca3d6b
Michal Ptaszek <michal@ptaszek.net>
2022-12-02 14:03:34 -0600

    Day02.2 done

--- 2022/day02/day02.c
+++ 2022/day02/day02.c
@@ -6,10 +6,13 @@ int selection_score(int shape)
     switch (shape)
     {
     case 'X':
+    case 'A':
         return 1;
     case 'Y':
+    case 'B':
         return 2;
     case 'Z':
+    case 'C':
         return 3;
     }
     return 0;
@@ -19,7 +22,6 @@ int result(char move, char response)
 {
     // A = rock, B = paper, C = scissors
     // X = rock, Y = paper, Z = scissors
-    // score
     if (move == 'A')
     {
         if (response == 'X')
@@ -51,6 +53,41 @@ int result(char move, char response)
     return 0;
 }
 
+int result2(char move, char expected_result)
+{
+    // A = rock, B = paper, C = scissors
+    // X = lose, Y = draw,  Z = win
+    if (move == 'A')
+    {
+        if (expected_result == 'X')
+            return selection_score('C');
+        if (expected_result == 'Y')
+            return selection_score('A') + 3;
+        else
+            return selection_score('B') + 6;
+    }
+    else if (move == 'B')
+    {
+        if (expected_result == 'X')
+            return selection_score('A');
+        if (expected_result == 'Y')
+            return selection_score('B') + 3;
+        else
+            return selection_score('C') + 6;
+    }
+    else if (move == 'C')
+    {
+        if (expected_result == 'X')
+            return selection_score('B');
+        if (expected_result == 'Y')
+            return selection_score('C') + 3;
+        else
+            return selection_score('A') + 6;
+    }
+
+    return 0;
+}
+
 int main()
 {
     FILE *fp;
@@ -68,7 +105,7 @@ int main()
         char move = line[0];
         char response = line[2];
 
-        score += result(move, response);
+        score += result2(move, response);
     }
 
     fclose(fp);
